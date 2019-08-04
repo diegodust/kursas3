1bfa6c18e0b92ff45e29edf8b34d226493447b01
diegodust <dangis1@yahoo.com>
2019-08-04 17:13:08 +0300

    Pridetas namu darbas js4

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
-function dalinti(x, y){
-	let resultatas = x / y;
-	document.write(resultatas);
+function arJumsYra18(pateikiamasAmzius){
+	
+	if(pateikiamasAmzius > 17){
+		document.write("Jūs galite užeiti į svetainę");
+	}
+	else{
+		document.write("Į svetainę užeiti negalite");
+	}
+	
 }
