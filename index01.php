eab79eab08d8d81a7ce25d9c8bc839f36519d41a
diegodust <dangis1@yahoo.com>
2019-08-18 14:22:54 +0300

    Pridetas namu darbas PHP1

---
+++ index01.php
@@ -0,0 +1,21 @@
+<?php $salis = 'Lietuvas'; ?>
+<?php $miestas = 'Vilnius'; ?>
+<?php $populiacija = 574221; ?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Namu darbas php1</title>
+</head>
+
+<body>
+	<ul>
+		<li><?php echo $salis; ?></li>
+		<li><?php echo $miestas; ?></li>
+		<li><?php echo $populiacija; ?></li>
+	</ul>
+				
+</body>
+</html>
